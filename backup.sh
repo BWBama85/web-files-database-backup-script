0f763603418ef29adbff1af5abd4cf4267ea902e
BWBama85 <41128321+BWBama85@users.noreply.github.com>
2023-03-07 17:55:13 -0600

    debug

--- backup.sh
+++ backup.sh
@@ -26,6 +26,9 @@ for dir in $(find "$DATA_DIR" -maxdepth 1 -type d); do
         if [ $DEBUG == "y" ]; then
             tar vzcf "$BACKUP_DIR/${dir##*/}-$TIMESTAMP.tar.gz" --exclude-from="$EXCLUDE_FILES" *
         fi
+        if [ $DEBUG == "y" ]; then
+            echo $?
+        fi
         if [ $? -ne 0 ]; then
             # There was an error creating the backup
             echo "[$(date +%Y-%m-%d\ %H:%M:%S)] - Error creating backup for $dir" | tee -a $LOG_FILE
