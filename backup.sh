c22094fce866ce2955cef8e88188f5b86d024e74
BWBama85 <41128321+BWBama85@users.noreply.github.com>
2023-03-07 16:14:26 -0600

    Fix

--- backup.sh
+++ backup.sh
@@ -20,7 +20,7 @@ for dir in $(find "$DATA_DIR" -maxdepth 1 -type d); do
     if [ "$dir" != "$DATA_DIR" ]; then
         echo "[$(date +%Y-%m-%d\ %H:%M:%S)] - Creating backup for $dir..." | tee -a $LOG_FILE
         cd "$dir"
-        tar -zcf$VERBOSE "$BACKUP_DIR/${dir##*/}-$TIMESTAMP.tar.gz" --exclude-from="$EXCLUDE_FILES" *
+        tar zcf$VERBOSE "$BACKUP_DIR/${dir##*/}-$TIMESTAMP.tar.gz" --exclude-from="$EXCLUDE_FILES" *
         if [ $? -ne 0 ]; then
             # There was an error creating the backup
             echo "[$(date +%Y-%m-%d\ %H:%M:%S)] - Error creating backup for $dir" | tee -a $LOG_FILE
